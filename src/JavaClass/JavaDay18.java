9db80de10843de65b20542198975adada3a84c81
vijayaprakash1709 <vijayaprakash885@gmail.com>
2024-07-04 11:29:04 +0530

    doubly circular

--- src/JavaClass/JavaDay18.java
+++ src/JavaClass/JavaDay18.java
@@ -62,63 +62,195 @@ package JavaClass;
 // }
 
 
-//-------------------------Doubly Linked List------------------
+//-------------------------Doubly Circular Linked List------------------
 
 public class JavaDay18 {
+    // Node class
     class Node{
-		int data;
-		Node next;
-		public Node(int d) {
-			this.data=d;
-			this.next=null;
-		}
-	}
-    public Node head=null;
-    public Node tail=null;
-
-    public void addNode(int data){
-        Node n=new Node(data);
-        if(head==null){
-            head=n;
-            tail=n;
+        int data;
+        Node next;
+        Node prev;
+        public Node(int d){
+            this.data=d;
+            this.next=null;
+            this.prev=null;
         }
-        else{
-            tail.next=n;
-            tail=n;
-            tail.next=head;
-        }
+ 
     }
+       //insert first last and print
+         public Node head=null;
+            public Node tail=null;
+            public void addNode(int data){
+                Node n=new Node(data);
+                if(head==null){
+                    head=n;
+                    tail=n;
+                    head.prev=null;
+                    tail.next=null;
+                }
+                else{
+                    tail.next=n;
+                    n.prev=tail;
+                    tail=n;
+                    tail.next=head;
+                    head.prev=tail;
+                }
+            }
+            public void print(){
+                Node curr=head;
+                if(head==null){
+                    System.out.println("List is empty");
+                }
+                else{
+                    do{
+                        System.out.print(curr.data + " -> ");
+                        curr=curr.next;
+                    }while(curr!=head);
+                    System.out.println();
+                }
+            }
+            
+            public void deleteLast(){
+                Node curr=head;
+                while(curr.next!=tail){
+                    curr=curr.next;
+                }
+                tail=curr;
+                tail.next=head;
+                head.prev=tail;
+            }
+            public void addFirst(int data){
+                Node n=new Node(data);
+                if(head==null){
+                    head=n;
+                    tail=n;
+                    head.prev=null;
+                    tail.next=null;
+                }
+                else{
+                n.next=head;
+                head.prev=n;
+                head=n;
+                head.prev=tail;
+                tail.next=head;
+            }}
+              public static void main(String[] args) {
+                JavaDay18 j=new JavaDay18();
+                j.addNode(20);
+                j.addNode(30);
+                j.addNode(10);
+                j.print();
+                j.deleteLast();
+                j.print();
+               }
 
-    public void print(){
-        Node curr=head;
-        if(head==null){
-            System.out.println("List is empty");
-        }
-        else{
-            do{
-                System.out.print(curr.data + " -> ");
-                curr=curr.next;
-            }while(curr!=head);
-            System.out.println();
-        }
-    }
-
-    public void deleteLast(){
-        Node curr=head;
-        while(curr.next!=tail){
-            curr=curr.next;
-        }
-        tail=curr;
-        tail.next=head;
-    }
-   public static void main(String[] args) {
-    JavaDay18 j=new JavaDay18();
-    j.addNode(20);
-    j.addNode(30);
-    j.addNode(10);
-    j.print();
-    j.deleteLast();
-    j.print();
-   }
-    
 }
+
+//----------------------Stack Using Linked List-------------------
+
+
+// public class JavaDay18 {
+//     class Node{
+// 		int data;
+// 		Node next;
+// 		public Node(int d) {
+// 			this.data=d;
+// 			this.next=null;
+// 		}
+// 	}
+//     public Node top=null;
+//     public void push(int data){
+//         Node n=new Node(data);
+//         if(top==null){
+//             top=n;
+//         }
+//         else{
+//             n.next=top;
+//             top=n;
+//         }
+//     }
+
+//     public void display(){
+//         Node curr=top;
+//         if(top==null){
+//             System.out.println("List is Empty");
+//         }
+//         else{
+//             while(curr!=null){
+//                 System.out.print(curr.data+" -> ");
+//                 curr=curr.next;
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public void pop(){
+//         if(top==null){
+//             System.out.println("List is Empty");
+//         }
+//         top=top.next;
+//     }
+ 
+//    public static void main(String[] args) {
+//     JavaDay18 j=new JavaDay18();
+//     j.push(10);
+//     j.push(20);
+//     j.push(30);
+//     j.display();
+//     j.pop();
+//     j.display();
+//    }  
+// }
+
+//-----------------------------Queue Implementation ----------------------
+
+// public class JavaDay18 {
+//     class Node{
+// 		int data;
+// 		Node next;
+// 		public Node(int d) {
+// 			this.data=d;
+// 			this.next=null;
+// 		}
+// 	}
+//     public Node top=null;
+//     public void push(int data){
+//         Node n=new Node(data);
+//         if(top==null){
+//             top=n;
+//         }
+//         else{
+//             n.next=top;
+//             top=n;
+//         }
+//     }
+
+//     public void display(){
+//         Node curr=top;
+//         if(top==null){
+//             System.out.println("List is Empty");
+//         }
+//         else{
+//             while(curr!=null){
+//                 System.out.print(curr.data+" -> ");
+//                 curr=curr.next;
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public void pop(){
+//         if(top==null){
+//             System.out.println("List is Empty");
+//         }
+//         top=top.next;
+//     }
+ 
+//    public static void main(String[] args) {
+//     JavaDay18 j=new JavaDay18();
+//     j.push(10);
+//     j.push(20);
+//     j.push(30);
+//     j.display();
+//     j.pop();
+//     j.display();
+//    }  
+// }
---
+++ src/JavaClass/JavaDay19.java
@@ -0,0 +1,41 @@
+package JavaClass;
+import java.util.Scanner;
+// public class JavaDay19 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         // armstrong number without using predefined methods
+//         int temp=n;
+//         int t=n;
+//         int sum=0;
+//         int count=0;
+//         while(n>0){
+//             n=n/10;
+//             count++;
+//         }
+//         n=temp;
+//         while(n>0){
+//             int rem=n%10;
+//             int p=1;
+//             for(int i=0;i<count;i++){
+//                 p=p*rem;
+//             }
+//             sum=sum+p;
+//             n=n/10;
+//         }
+//         if(t==sum){
+//             System.out.println("Armstrong number");
+//         }
+//         else{
+//             System.out.println("Not an armstrong number");
+//         }
+//     }
+// }
+
+//------------------------------Tree--------------------------------
+
+public class JavaDay19 {
+    public static void main(String[] args) {
+        
+    }
+}
